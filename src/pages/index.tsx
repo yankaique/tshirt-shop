0d3152cf0e53dfc3e147361a161e27a2130b30b8
Yan Marinho <yankaiquecosta.yk@gmail.com>
2023-02-03 06:59:41 -0300

    ✨ add stitches and finish global style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
+import { styled } from "@/styles"
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '10 10 10 10',
+  span: {
+    fontWeight: 'bold'
+  },
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  }
+})
+
 export default function Home() {
   return (
     <>
       <h1>Hello World</h1>
+      <Button>
+        <span>Teste</span>
+        Enviar
+        </Button>
     </>
   )
 }
