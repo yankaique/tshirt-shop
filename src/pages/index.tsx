5009b3c073c4935dca996a28f16a7b30db7b3404
Yan Marinho <yankaiquecosta.yk@gmail.com>
2023-02-06 08:12:07 -0300

    🚧 add carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,50 @@
 import { HomeContainer, Product } from "@/styles/pages/home"
+import { useKeenSlider } from 'keen-slider/react'
 import Image from "next/image"
 
 import shirtOne from '@/assets/shirts/1.png'
 import shirtTwo from '@/assets/shirts/2.png'
 import shirtThree from '@/assets/shirts/3.png'
+import 'keen-slider/keen-slider.min.css'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={shirtOne} alt="" width={520} height={480}/>
-        {/* <Image src={shirtTwo} alt="" width={520} height={480}/>
-        <Image src={shirtThree} alt="" width={520} height={480}/> */}
         <footer>
           <strong>Camiseta X</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
 
-      <Product>
-        {/* <Image src={shirtOne} alt="" width={520} height={480}/> */}
+      <Product className="keen-slider__slide">
         <Image src={shirtTwo} alt="" width={520} height={480}/>
-        {/* <Image src={shirtThree} alt="" width={520} height={480}/> */}
         <footer>
           <strong>Camiseta Y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+       <Image src={shirtThree} alt="" width={520} height={480}/>
+        <footer>
+          <strong>Camiseta Z</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+       <Image src={shirtThree} alt="" width={520} height={480}/>
+        <footer>
+          <strong>Camiseta Z</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
